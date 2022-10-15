209e0999f0013fe92d63afdadf6c21bbc367f8ef
Agent200147 <91262640+Agent200147@users.noreply.github.com>
2022-10-16 01:33:45 +0300

    add swiper and some styles

--- js/index.js
+++ js/index.js
@@ -1,19 +1,96 @@
-const currentTimeContainer = document.querySelector('.timer__current-time')
+// const currentTimeContainer = document.querySelector('.timer__current-time')
+//
+// setInterval(() => {
+//     let date = new Date()
+//
+//     let hours
+//     let minutes
+//     let seconds
+//
+//     date.getHours().toString().length === 2 ? hours = date.getMinutes() : hours = "0" + date.getHours()
+//     date.getMinutes().toString().length === 2 ? minutes = date.getMinutes() : minutes = "0" + date.getMinutes()
+//     date.getSeconds().toString().length === 2 ? seconds = date.getSeconds() : seconds = "0" + date.getSeconds()
+//
+//     let currentTime
+//
+//     currentTime = `${date.getHours()}:${minutes}:${seconds}`
+//     currentTimeContainer.innerHTML = currentTime
+// }, 1000)
 
-setInterval(() => {
-    let date = new Date()
+const timer__hours = document.querySelector('.swiper-wrapper__hours')
+const timer__minutes = document.querySelector('.swiper-wrapper__minutes')
+const timer__seconds = document.querySelector('.swiper-wrapper__seconds')
 
-    let hours
-    let minutes
-    let seconds
+for (let i = 0; i < 24; i++) {
+    i < 10 ? i = '0' + i : i
+    timer__hours.innerHTML += `<div class="timer__hours-item swiper-slide">${i}</div>`
+}
 
-    date.getHours().toString().length === 2 ? hours = date.getMinutes() : hours = "0" + date.getHours()
-    date.getMinutes().toString().length === 2 ? minutes = date.getMinutes() : minutes = "0" + date.getMinutes()
-    date.getSeconds().toString().length === 2 ? seconds = date.getSeconds() : seconds = "0" + date.getSeconds()
+for (let i = 0; i < 60; i++) {
+    i < 10 ? i = '0' + i : i
+    timer__minutes.innerHTML += `<div class="timer__minutes-item swiper-slide">${i}</div>`
+}
 
-    let currentTime
+for (let i = 0; i < 60; i++) {
+    i < 10 ? i = '0' + i : i
+    timer__seconds.innerHTML += `<div class="timer__seconds-item swiper-slide">${i}</div>`
+}
 
-    currentTime = `${date.getHours()}:${minutes}:${seconds}`
-    currentTimeContainer.innerHTML = currentTime
-}, 1000)
+// $(function(){
+//     $('.timer__item').slick({
+//         vertical: true,
+//         verticalSwiping: true,
+//         arrows: false,
+//         dots: false,
+//         infinite: true,
+//
+//         autoplay: false,
+//         // autoplaySpeed: 5000,
+//         // touchThreshold: 10,
+//         // speed: 200,
+//         pauseOnFocus: false,
+//         pauseOnHover: false,
+//
+//         slidesToShow: 3,
+//         // slidesToScroll: auto,
+//
+//         swipe: true,
+//         swipeToSlide: true,
+//         // TouchMove: true,
+//         // centerMode: true,
+//         // centerPadding: '60px',
+//         responsive: [
+//             {
+//                 breakpoint: 576,
+//                 settings: {
+//                     speed: 200,
+//                     autoplay: false,
+//                     infinite: false,
+//                 }
+//             },
+//         ]
+//     })
+//
+// })
+
+var mySwiper = new Swiper('.swiper', {
+    direction: 'vertical',
+    speed: 500,
+    slidesPerView: 5,
+    loopedSlides: 60,
+    // spaceBetween: 65,
+    loop: true,
+    freeMode: {
+        enabled: true,
+        sticky: true,
+    },
+    //
+    // effect: 'coverflow',
+    // coverflowEffect: {
+    //     rotate: 30,
+    //     slideShadows: false,
+    //     depth: 100,
+    // },
+});
+
 
